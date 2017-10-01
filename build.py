da7dd056c1a4e104794e1d866626af2d21c3cba5
sgFlags <bwhcolin@outlook.com>
2017-10-01 16:56:00 -0400

    pybuilder

---
+++ build.py
@@ -0,0 +1,10 @@
+from pybuilder.core import use_plugin
+from pybuilder.core import init
+
+use_plugin("python.core")
+
+@init
+def initialize(project):
+    project.set_property('dir_source_main_python', 'blog')
+
+default_task = "publish"
