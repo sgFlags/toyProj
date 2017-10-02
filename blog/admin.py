346c0a3ea1df296d4a0429d35f90bcc83af5bc07
sgFlags <bwhcolin@outlook.com>
2017-10-02 18:12:07 -0400

    toyalmost

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'created_date', 'published_date', 'updated_date', 'status')
+    list_filter = ('created_date', 'published_date', 'author')
+    search_fields = ('title', 'text')
+
+admin.site.register(Post, PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 # Create your views here.
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+		status='published',
+		published_date__year=year,
+		published_date__month=month,
+		published_date__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
